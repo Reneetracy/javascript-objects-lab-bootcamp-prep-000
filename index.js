908fd04f9be4c8335ea5a5e1a8576b7c8aceec74
Renee Tracy <renee.upandaway@gmail.com>
2017-09-11 06:11:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var recipes = { ingredient: 'amount' };
 
 function updateObjectWithKeyAndValue(recipes, ingredient, amount){
   var newRecipe = object.assign({}, recipes);
+  recipes[ingredient] = amount;
   return newRecipe
 }
 
