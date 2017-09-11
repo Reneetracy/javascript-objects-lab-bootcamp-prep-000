bc9f88dea9b0d4660088fac1545cbda828ce4e19
Renee Tracy <renee.upandaway@gmail.com>
2017-09-11 06:26:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ function updateObjectWithKeyAndValue(recipes, ingredient, amount){
   return Object.assign({}, recipes, { [ingredient]: amount })
 }
 
-
 function destructivelyUpdateObjectWithKeyAndValue(recipes, ingredient, amount){
   recipes[ingredient] = amount;
   return recipes
 }
+
+
